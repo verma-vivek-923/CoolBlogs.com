602926bc4b1675d728adc6ecfdf55beba8b23b49
verma-vivek-923 <verma022003@gmail.com>
2025-01-24 23:01:51 +0530

    correct  frontend cookie get methods

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -12,7 +12,7 @@ export const  createTokenAndSaveCookies=async(userId,res)=>{
         secure:true,
         path:"/",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 2 days in milliseconds
-        domain: "https://cool-blogs-com.vercel.app",
+        
     })
     await user.findByIdAndUpdate(userId,{token})
     return token;
