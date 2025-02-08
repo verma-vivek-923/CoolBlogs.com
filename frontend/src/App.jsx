b47ca06ab928996041e92c4d50b2de908538b517
verma-vivek-923 <verma022003@gmail.com>
2025-02-08 14:24:40 +0530

    search feture initializ

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import Update_Blog from './dashboard/Update_Blog'
 import Update_user from './dashboard/Update_user'
 import Detail from './pages/Detail'
 import ForgotPassword from './pages/ForgotPassword'
+import Search_blogs from './pages/Search_blogs'
 
 function App() {
   const location=useLocation();
---
+++ frontend/src/pages/Search_blogs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search_blogs = () => {
+  return (
+    <div>search_blogs</div>
+  )
+}
+
+export default Search_blogs
